f97c31a7ea1ff3fc4c83396649c1f9f8ed0340e5
Marko Ivanetić <MarkoIvanetic@users.noreply.github.com>
2021-09-12 11:04:07 +0200

    Update national_coding_week.js

--- national_coding_week.js
+++ national_coding_week.js
@@ -4,21 +4,20 @@
 // challenge:
 // https://app.codility.com/programmers/challenges/national_coding_week_2021/
 
-function replaceFirstOccurrence (s, f = "abb", r = "baa") {
-    // From the given string s, replace f with r of nth occurrence
-    return s.replace(f, r);
-};
-
-function getBestMutation(str) {
-    const mutation = replaceFirstOccurrence(str)
-    if (mutation !== str) {
-        return getBestMutation(replaceFirstOccurrence(mutation))
-    }
-    return str
+function transform(s) {
+    return s.replace(/abb/g, 'baa');
 }
-
 
 function solution(S) {
     // write your code in JavaScript (Node.js 8.9.4)
-    return getBestMutation(S)
+
+    let best = S
+    let next = transform(S)
+
+    while (best !== next) {
+        best = next
+        next = transform(next)
+    }
+    return best
+
 }
