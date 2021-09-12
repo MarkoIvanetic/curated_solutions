b31452e8759cf249914d901e02a87de4698d85d9
Marko Ivanetić <MarkoIvanetic@users.noreply.github.com>
2021-09-12 10:52:00 +0200

    Update national_coding_week.js

--- national_coding_week.js
+++ national_coding_week.js
@@ -4,50 +4,21 @@
 // challenge:
 // https://app.codility.com/programmers/challenges/national_coding_week_2021/
 
-function replaceOccurrence (s, n, f = "abb", r = "baa") {
+function replaceFirstOccurrence (s, f = "abb", r = "baa") {
     // From the given string s, replace f with r of nth occurrence
-    return s.replace(RegExp("^(?:.*?" + f + "){" + n + "}"), x => x.replace(RegExp(f + "$"), r));
+    return s.replace(f, r);
 };
 
-function getNoOccurrences(s, frag = "abb") {
-    return s.split(frag).length - 1
-}
-
-function hasOccurrence(s, frag = "abb") {
-    return s.includes(frag)
-}
-
 function getBestMutation(str) {
-    if (hasOccurrence(str)) {
-        return getBestMutation(replaceOccurrence(str, 1))
+    const mutation = replaceFirstOccurrence(str)
+    if (mutation !== str) {
+        return getBestMutation(replaceFirstOccurrence(mutation))
     }
     return str
 }
 
-// function getPossibleStringMutations(str) {
-//     const mutations = []
-
-//     for (let i = 1; i <= getNoOccurrences(str); i++) {
-//         const mutation = replaceOccurrence(str, i)
-//         mutations.push(mutation)
-
-//         if (hasOccurrence(mutation)) {
-//             const childMutations = getPossibleStringMutations(mutation)
-//             mutations.push(...childMutations)
-//         }
-//     }
-
-//     return mutations
-// }
 
 function solution(S) {
     // write your code in JavaScript (Node.js 8.9.4)
-    if (!hasOccurrence(S)) {
-        return S
-    }
-
-    // const mutationArray = getPossibleStringMutations(S)
-    // return mutationArray.sort().pop()
-
     return getBestMutation(S)
 }
