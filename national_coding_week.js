3d3bae3094995da1ea88af5b4c419dbd5ab3751e
Marko Ivanetić <MarkoIvanetic@users.noreply.github.com>
2021-09-13 19:52:59 +0200

    Update national_coding_week.js

--- national_coding_week.js
+++ national_coding_week.js
@@ -4,6 +4,45 @@
 // challenge:
 // https://app.codility.com/programmers/challenges/national_coding_week_2021/
 
+// 100% solution:
+function matchesPattern(data, i) {
+    return data[i] === 'a' && data[i + 1] === 'b' && data[i + 2] === 'b'
+}
+
+function transform(data, i) {
+    data[i] = 'b'
+    data[i + 1] = 'a'
+    data[i + 2] = 'a'
+}
+
+function hasUnfinishedBuisness(data, i) {
+    return data[i - 2] === 'a' && data[i - 1] === 'b'
+}
+
+function solution(S) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    const data = S.split('')
+
+    for (let i = 0, matched = false; i < data.length; matched = false) {
+        if (matchesPattern(data, i)) {
+            transform(data, i)
+            matched = true
+        }
+
+        if (matched && hasUnfinishedBuisness(data, i)) {
+            // go back 2 indexes behind, to solve newly created 'abb' pattern
+            i -= 2
+        } else {
+            i += 1
+        }
+    }
+
+    return data.join('')
+}
+
+
+// *******************************************************
+// first solution (caused timeouts)
 function transform(s) {
     return s.replace(/abb/g, 'baa');
 }
@@ -20,4 +59,24 @@ function solution(S) {
     }
     return best
 
+}
+
+// *******************************************************
+// string optimization for long patterns
+// solve all /(abb)([b]+) patterns as they are too costy
+// caused timeout on big_simple test?
+function minifyString(s) {
+   return s.replace(/(abb)([b]+)/g, function (t0, t1, t2) {
+       // x is the number of 'ba' susbtrings that localized solution will have
+        let x = Math.floor((t2.length + 2)  / 2)
+        let newSeq = new Array(x).fill("ba").join("")
+
+        if (t2.length % 2 === 0) {
+            newSeq += "a"
+        } else {
+            newSeq += "ab"
+        }
+
+        return newSeq
+    })
 }
